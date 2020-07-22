210ae647ea81555f71b4df23a9077969b0bd277c
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-07-22 15:24:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const webpack = require('webpack');
+// const WebpackDevServer = require('webpack-dev-server/lib/Server');
+const config = require('./webpack.config');
+
+fs.writeFileSync('main.css', 'body { background-color: red; }');
+
+const compiler = webpack(config);
+// const server = new WebpackDevServer(compiler, {
+//   hot: true,
+//   noInfo: true,
+// });
+
+compiler.hooks.done.tap('test', () => {
+  console.log('done');
+});
+
+compiler.watch({
+  aggregateTimeout: 300,
+}, () => {
+  console.log('watch callback');
+});
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './foo.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+};
